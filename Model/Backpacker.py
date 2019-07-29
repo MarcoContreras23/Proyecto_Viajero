11c77d42040e4726a52ea230bdec56a26c0f5357
Manuela Rendon Rendon <manuela.170172099@ucaldas.edu.co>
2019-07-29 01:42:59 -0500

    actual

--- Model/Backpacker.py
+++ Model/Backpacker.py
@@ -1,4 +1,7 @@
 class Backpacker:
-    def __init__(self):
-        self.budget = 0
-        self.time = 0
+    def __init__(self,budget,time):
+        self.budget = budget
+        self.time = time
+        self.porcentaje_minimo = budget * 0.4
+
+
--- Model/algorithms.py
+++ Model/algorithms.py
@@ -6,6 +6,7 @@ from math import inf
 class Algorithms:
     def __init__(self):
         self.routeEconomica =[]
+        self.trabajo =  False
 
     def BFS(self, visited, trail):
         if len(trail) == 0:
@@ -59,7 +60,7 @@ class Algorithms:
         visitPlaces = self.Dijkstra(
             minplace, places, conection, edgesOrigin, state, visitPlaces,minCost,minTime)
         return visitPlaces
-
+#se llama despues de dijkstra para que cree la ruta
     def llenarRoute(self,origen,backpacker):
         mayor = 0
         for i in self.place:
@@ -79,16 +80,58 @@ class Algorithms:
         self.routeEconomica = tem
 
 #punto 3
-    def restar_costos(self, origen, destino,transporte,actividades,backpacker):
+
+# llamar para saber si se envian atributos trabajos y time_trabajo  en la siguiente funcion
+    def evaluar_presupuesto(self, backpacker):
+        if backpacker.budget <= backpacker.porcentaje_minimo:
+            return True
+        else:
+            return False
+#se llama cuando el mochilero comience a seguir la ruta por cada vertice que avance
+    def empezar_viaje(self, origen, destino, transporte, actividades, backpacker, trabajos, time_trabajo):
+        self.restar_costos(origen, destino,transporte,actividades,backpacker, trabajos, time_trabajo)
+
+#como el mochilero no puede dormir mientras viaja o hace una actividad se debe tener en cuenta de que si el tiempo que demoro lo que hacia el mochilero
+#paso las 18 horas el debe dormir al terminar
+    def restar_costos(self, origen, destino,transporte,actividades,backpacker, trabajo, time_trabajo):
         restarCosto = 0
         restarTiempo = 0
+        nodo_evaluado =None
+        numero_dormidas=0
+        numero_comidas = 0
         for v in origen.adjacencies:
             if v.destiny is destino:
+                nodo_evaluado =v.destiny
                 restarCosto += v.Distance*transporte.value
                 restarTiempo += v.distance*transporte.time
-        for t in actividades:
-            restarCosto += t.cost
-            restarTiempo += t.time
+                if v.distance*transporte.time // 18 is not 0:
+                    numero_dormidas += 1 #solo 1 porque asi cumpla varias veces la hora mientras viaja no puede dormir
+                if v.distance*transporte.time // 6 is not 0:
+                    numero_comidas += v.distance*transporte.time // 6 #se multiplica por todas las veces que se cumplieron las 6 horas porque puede comer asi este haciendo algo
+
+        if actividades is not None:
+            for t in actividades:
+                restarCosto += t.cost
+                restarTiempo += t.time
+                if t.time // 18 is not 0:
+                    numero_dormidas += 1
+                if t.time // 6 is not 0:
+                    numero_comidas += t.time // 6
+
+        if trabajo is not None:
+                restarCosto += trabajo.gain*time_trabajo
+                restarTiempo += time_trabajo
+                if time_trabajo // 18 is not 0:
+                    numero_dormidas += 1
+                if time_trabajo // 6 is not 0:
+                    numero_comidas += time_trabajo // 6
+        for obli in nodo_evaluado.task:
+            if obli.name is "Sleep":
+                restarCosto += obli.cost*numero_dormidas
+                restarTiempo += obli.time *numero_dormidas
+            if obli.name is "Eat":
+                restarCosto += obli.cost * numero_comidas
+                restarTiempo += obli.time * numero_comidas
 
         backpacker.budget -= restarCosto
         backpacker.time -= restarTiempo
